368bbe6d5c579bc8ef89fb79e8cea57bee899581
Kerim Kurttepeli <kerimkurttepeli@Kerims-MacBook-Pro.local>
2020-03-19 19:47:16 +0300

    Modified main so that it checks the order and the position of the characters

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import sys
 
 def is_one_to_one(s1: str, s2: str) -> bool:
-    if len(s1) > len(s2):
+    if len(s1) != len(s2):
         return False
     else:
-        return len(set(s1)) >= len(set(s2))
-
+        char_map = {}
+        for i,c1 in enumerate(s1):
+            if c1 not in char_map:
+                char_map[c1] = ord(s2[i])
+            elif char_map[c1] != ord(s2[i]):
+                return False
+        return True
 
 
 if __name__ == '__main__':
-    #error handling for illegal command line arguments not dealt with
+    if len(sys.argv) != 3:
+        print("usage: python main.py <string1> <string2>",file=sys.stderr)
+        exit()
     s1 = sys.argv[1]
     s2 = sys.argv[2]
     if is_one_to_one(s1,s2):
